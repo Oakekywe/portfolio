206e472201e3371af83e236637092f1c698f1ef9
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-24 21:03:44 +0630

    add button new skills and projects

--- resources/js/Pages/Projects/Create.vue
+++ resources/js/Pages/Projects/Create.vue
@@ -4,19 +4,16 @@
 </script>
 
 <template>
-  <Head title="Skills Index" />
+  <Head title="New Project" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Skills</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">New Project</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-end m-2 p-2">
-                    <Link :href="route('skills.create')" type="button"
-                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">New Skill</Link>
-                </div>
+                Form2
             </div>
         </div>
     </AuthenticatedLayout>
