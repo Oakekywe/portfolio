ef75254f5ed8a4b4d41cd126e3719bd31be6d558
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-27 21:24:32 +0630

    adding frontend design by tailwind flowbite

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,20 +1,17 @@
 <script setup>
+import FrontendVue from '@/Layouts/Frontend.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import Frontend from '@/Layouts/Frontend.vue'
 
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-});
 </script>
 
 <template>
     <Head title="Portfolio" />
-
+    <Frontend> </Frontend>
     
 </template>
 
+<!--
 <style>
 .bg-dots-darker {
     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
@@ -25,3 +22,4 @@ defineProps({
     }
 }
 </style>
+-->
